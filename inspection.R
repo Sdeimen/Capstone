b2b56a079407fb5ada53aacb78b91631ca5631bc
Sebastian Deimen <SebastianDeimen@gmx.de>
2021-04-01 20:53:30 -0700

    two more R files, plotting and insprection

---
+++ inspection.R
@@ -0,0 +1,85 @@
+# MS Information Capstone 
+# Sebastian Deimen
+# STEM opportunities in the US
+# University of Arizona - Spring 2021
+#
+# Purpose: Inspecting the chosen variables
+
+setwd("H:/MyDropbox/Dropbox/A_UofA/INFO 698/Capstone")
+# install.packages("rockchalk")
+# loading packages
+library(tidyverse)
+library(rockchalk) # for combinig levels
+
+load("data/opps.RData")
+
+# for inspecting purposes I removed the three text attributes "description", excerpt" and "title"
+opps_inspect <- select(opps, -title, -excerpt, -description)
+glimpse(opps_inspect)
+
+# checking for incomplete cases
+nrow(filter(opps_inspect, !complete.cases(opps_inspect)))
+# it looks like 11 cases are incomplete
+filter(opps_inspect, !complete.cases(opps_inspect))
+# and those seems to have missing Longs and Lats, which is not super important, so I will keep those
+# There are a couple of other missing data, such as "attention", but for now I will keep those as well
+
+
+# Summmary showed a couple of double appearances of "titles" - after closer inspection those are the same opportunity but with different dates, different locations or different areas of interest
+opps[296,] == opps[319,] # Title: Brick Flicks (Ages 9-13) - Northville, MI
+opps[243,] == opps[261,] # Title: Chowdhury STEM Innovation Contest
+
+
+# adding a duration column ----
+# transform the fromDate and toDate into a Date format to calculate the difference 
+opps_inspect$fromDate <- as.POSIXct(opps_inspect$fromDate, format = "%Y-%m-%d %H:%M") 
+#check 
+opps_inspect$fromDate
+opps_inspect$toDate <- as.POSIXct(opps_inspect$toDate, format = "%Y-%m-%d %H:%M")
+# check
+opps_inspect$toDate
+#  creating the duration column
+opps_inspect$duration <- as.integer(opps$toDate - opps$fromDate)
+(opps_inspect$duration)
+ggplot(opps_inspect, aes(x=duration)) +
+  geom_histogram()
+
+# Costs ----
+# exploring costs - Intervalls: 10 Levels, two duplicates due to typos, combining them togehter
+opps_inspect$cost <-  combineLevels(opps_inspect$cost, c("More than $1,000","More than $1000"), newLabel = "More than $1,000")
+opps_inspect$cost <-  combineLevels(opps_inspect$cost, c("$25 or less","$25 or Less"), newLabel = "$25 or less")
+opps_inspect$cost <-  combineLevels(opps_inspect$cost, c("","Free"), newLabel = "Free")
+
+# order the levels for plotting
+opps_inspect_cost_ordered <- opps_inspect
+opps_inspect_cost_ordered$cost <- factor(opps_inspect_cost_ordered$cost, levels =c("Free", "$25 or less", "$26-$50", "$51-$100", "$101-$500", "$501-$1,000", "More than $1,000"))
+
+# plot to see the distribution of opportunity costs
+ggplot(opps_inspect_cost_ordered, aes(cost, fill = cost)) +
+  geom_bar() +
+  theme(axis.title = element_text(size = rel(1.8))) +
+  labs(x = "Cost intervals") +
+  theme_classic() +
+  theme(legend.position = "none", axis.text.x = element_text(angle = 90))
+
+# see if costs and scholarship are somehow related
+opps_inspect_cost_ordered$cost <-  combineLevels(opps_inspect_cost_ordered$cost, c("","False"), newLabel = "False")
+opps_inspect_cost_ordered$scholarship <-  combineLevels(opps_inspect_cost_ordered$scholarship, c("","False"), newLabel = "False")
+cost_scholars <- select(opps_inspect_cost_ordered, cost, scholarship)
+table(cost_scholars)
+
+
+ggplot(cost_scholars, aes(cost, fill = cost, alpha = scholarship)) + 
+  geom_bar() +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90))
+# as we can see, only the price range "$100 to $500" offers a lot of scholarships
+
+
+
+
+# Ages ----
+
+ages <- opps_inspect$ages
+ages
+
---
+++ plotting_maps.R
@@ -0,0 +1,144 @@
+# MS Information Capstone 
+# Sebastian Deimen
+# STEM opportunities in the US
+# University of Arizona - Spring 2021
+#
+# Purpose: Plotting maps
+
+library(tidyverse)
+library(ggmap)
+library(maps)
+library(mapdata)
+library(stringr)
+
+load("data/opps.RData")
+# for inspecting purposes I removed the three text attributes "description", excerpt" and "title"
+opps_inspect <- select(opps, -title, -excerpt, -description)
+
+
+# Maps ----
+
+# install.packages(c("maps", "mapdata"))
+# install.packages("Rtools")
+# devtools::install_github("dkahle/ggmap")
+
+
+# load mainland US, states and counties map
+usa <- map_data("usa")
+states <- map_data("state")
+counties <- map_data("county")
+
+# plot mainland US with the opportunities locations
+ggusa <- ggplot() +
+  geom_polygon(data = usa, aes(x=long, y=lat, group=group)) +
+  coord_fixed(1.3)
+ggusa +
+  geom_point(data= opps_inspect, aes(x = locationLongitude, y = locationLatitude), color = "red", size = 1)
+
+# opportunity 18 based in Stockholm, remove, the other two outliers are Honululu and San Jose
+opps_inspect <- opps_inspect[-c(18),]
+
+# plot with State boundaries
+ggstates <- ggplot() + 
+  geom_polygon(data = states, aes(x = long, y = lat, fill = region, group = group), color = "white") + 
+  coord_fixed(1.3) +
+  guides(fill=FALSE)  # do this to leave off the color legend
+ggstates + 
+  geom_point(data= opps_inspect, aes(x = locationLongitude, y = locationLatitude), color = "red", size = 1)
+
+
+# possibility to zoom in on US mape
+zoom_us <- ggstates + 
+  geom_point(data= opps_inspect, aes(x = locationLongitude, y = locationLatitude), color = "royalblue1", size = 2,inherit.aes = FALSE)
+zoom_us
+zoom_us + coord_fixed(xlim = c(-83, -68),  ylim = c(33, 46), ratio = 1.3)
+
+
+# zoom in to a couple of states
+# California ----
+california_base <- subset(states, region %in% c("california"))
+
+
+ggcali <- ggplot(data = california_base) +
+  geom_polygon(aes(x=long, y=lat, group = group), fill = "sandybrown", color="black") + 
+  coord_fixed(1.3)
+# filter opps in California
+opps_california <- filter(opps_inspect, locationState == "California")
+ggcali + 
+  geom_point(data= opps_california, aes(x = locationLongitude, y = locationLatitude), color = "royalblue1", size = 2)
+# get the counties
+
+ca_county <- subset(counties, region == "california")
+ggcali_county <- ggplot(data = ca_county, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color = "black", fill = "gray")
+
+#zoom in a bit
+zoom_ca <- ggcali_county + 
+  geom_point(data= opps_california, aes(x = locationLongitude, y = locationLatitude), color = "royalblue1", size = 2,inherit.aes = FALSE)
+zoom_ca
+
+
+zoom_ca + coord_fixed(xlim = c(-122.8, -121.5),  ylim = c(36.8, 38), ratio = 1.3)
+
+
+
+
+
+# Ohio ----
+ohio_base <- subset(states, region %in% c("ohio"))
+ggohio <- ggplot() +
+  geom_polygon(data = ohio_base, aes(x=long, y=lat, group = group), fill = "purple", color="black") + 
+  coord_fixed(1.3)
+# filter opps in Ohio
+opps_ohio <- filter(opps_inspect, locationState == "Ohio")
+ggohio + 
+  geom_point(data= opps_ohio, aes(x = locationLongitude, y = locationLatitude), color = "orange", size = 2)
+
+
+# get the counties
+
+ohio_county <- subset(counties, region == "ohio")
+ggohio_county <- ggplot(data = ohio_county, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color = "black", fill = "gray")
+
+
+
+zoom_ohio <- ggohio_county + 
+  geom_point(data= opps_ohio, aes(x = locationLongitude, y = locationLatitude), color = "royalblue1", size = 2,inherit.aes = FALSE)
+zoom_ohio
+#zoom in a bit
+zoom_ohio + coord_fixed(xlim = c(-85, -84),  ylim = c(39, 40), ratio = 1.3)
+
+
+
+
+# Plotting function, state name HAS to be a lowercase string ----
+base_plots <- function(state)  {
+  base <- subset(states, region %in% state)
+  ggstate <- ggplot() +
+    geom_polygon(data = base, aes(x=long, y=lat, group = group), fill = "purple", color="black") + 
+    coord_fixed(1.3)
+  # filter opps in Ohio
+  opps_state <- filter(opps_inspect, locationState == str_to_title(state))
+  gg_color <- ggstate + 
+                geom_point(data= opps_state, aes(x = locationLongitude, y = locationLatitude), color = "orange", size = 2)
+  gg_color
+  
+  # get the counties
+  
+  state_county <- subset(counties, region == state)
+  ggstate_county <- ggplot(data = state_county, mapping = aes(x = long, y = lat, group = group)) + 
+    coord_fixed(1.3) + 
+    geom_polygon(color = "black", fill = "gray")
+  
+  zoom_state <- ggstate_county + 
+    geom_point(data= opps_state, aes(x = locationLongitude, y = locationLatitude), color = "royalblue1", size = 2,inherit.aes = FALSE)
+  zoom_state
+  # zoom in a bit - longs and lats are too specific to use in a function
+  # zoom_state + coord_fixed(xlim =  c(-80, -77),  ylim = c(35, 36), ratio = 1.3)
+  
+  }
+
+
