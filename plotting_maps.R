e94e327938ec46e9b8d05c213716bccadb90fa1d
Sebastian Deimen <SebastianDeimen@gmx.de>
2021-04-19 13:47:51 -0700

    additional data for plotting maps and the data scraping file

--- plotting_maps.R
+++ plotting_maps.R
@@ -10,6 +10,7 @@ library(ggmap)
 library(maps)
 library(mapdata)
 library(stringr)
+library(hablar) # turning factors into doubles...
 
 
 load("data/opps.RData")
@@ -74,18 +75,25 @@ base_plots <- function(state)  {
   # get the counties
   opps_state <- filter(opps_inspect, locationState == str_to_title(state))
   state_county <- subset(counties, region == state)
+  state_by_lat <- data.frame(table(opps_state$locationLatitude))
+  colnames(state_by_lat) <- c("locationLatitude", "Freq")
+  
+  state_by_lat$locationLatitude <- as_reliable_num(state_by_lat$locationLatitude)
+  opps_state <- inner_join(opps_state, state_by_lat, by = "locationLatitude")
+  
   ggplot(data = state_county, mapping = aes(x = long, y = lat, group = group)) + 
     coord_fixed(1.3) + 
     geom_polygon(color = "black", fill = "gray") +
-    geom_point(data= opps_state, aes(x = locationLongitude, y = locationLatitude), color = "royalblue1", size = 2,inherit.aes = FALSE) +
-    ggtitle(str_to_title(state))
+    geom_point(data= opps_state, aes(x = locationLongitude, y = locationLatitude, size = Freq), color = "royalblue1",inherit.aes = FALSE) +
+    ggtitle(str_to_title(state), "Opportunity Overview")
   # zoom in a bit - longs and lats are too specific to use in a function
   # zoom_state + coord_fixed(xlim =  c(-80, -77),  ylim = c(35, 36), ratio = 1.3)
   
 }
 base_plots("arizona")  
-
-
+base_plots("north carolina")
+base_plots("indiana")
+base_plots("texas")
 # Try some plots by hand, result is the function above ----
 # zoom in to a couple of states
 # California ----
@@ -161,36 +169,68 @@ zoom_ohio + coord_fixed(xlim = c(-85, -84),  ylim = c(39, 40), ratio = 1.3)
 
 # Bringing in some extra data ----
 
+add_data_plot <- function(state, data, title){# state HAS to be lower case,  data = read_csv file, title= Plot title
+  
+  # convert the counties data to Title case
+  state_counties <- subset(counties, region == state)
+  state_counties$subregion <- str_to_title(state_counties$subregion)
+  opps_state <- filter(opps_inspect, locationState == str_to_title(state))
+  state_by_lat <- data.frame(table(opps_state$locationLatitude))
+  colnames(state_by_lat) <- c("locationLatitude", "Freq")
+  
+  
+  state_by_lat$locationLatitude <- as_reliable_num(state_by_lat$locationLatitude)
+  opps_state <- inner_join(opps_state, state_by_lat, by = "locationLatitude")
+  
+  nc_complete <- inner_join(state_counties, data, by ="subregion")
+  
+  state_base <- ggplot(data = state_counties, mapping = aes(x = long, y = lat, group = group)) + 
+    coord_fixed(1.3) + 
+    geom_polygon(color = "black", fill = "dodgerblue4")
+  
+  state_plot <- state_base + 
+    geom_polygon(data = nc_complete, aes(fill = numbers), color = "white") +
+    geom_polygon(color = "black", fill = NA) +
+    theme_bw() +
+    ggtitle(str_to_title(state), title) 
+  
+  state_plot + scale_fill_gradientn(colours = rev(rainbow(7))) + 
+    geom_point(data= opps_state, aes(x = locationLongitude, y = locationLatitude, size=Freq), color = "black",inherit.aes = FALSE) 
+}
+
 nc_pov <- read.csv("data/north_carolina_poverty.csv", colClasses=c("NULL", NA, NA))
-# Colnames are named wrong, redo it
-colnames(nc_pov) <- c("subregion", "percentage")
-# convert the counties data to Title case
-nc_counties <- subset(counties, region %in% c("north carolina"))
-nc_counties$subregion <- str_to_title(nc_counties$subregion)
-opps_north_carolina <- filter(opps_inspect, locationState == "North Carolina")
-
-# inner_join
-nc_complete <- inner_join(nc_counties, nc_pov, by ="subregion")
-               
-nc_complete
-
-
-
-# plot
-nc_base <- ggplot(data = nc_counties, mapping = aes(x = long, y = lat, group = group)) + 
-  coord_fixed(1.3) + 
-  geom_polygon(color = "black", fill = "dodgerblue4")
-
-nc_pov_plot <- nc_base + 
-  geom_polygon(data = nc_complete, aes(fill = percentage), color = "white") +
-  geom_polygon(color = "black", fill = NA) +
-  theme_bw() +
-  ggtitle(str_to_title("North caroliona by Poverty Rate")) + # adding the blue/white color 
-  geom_point(data= opps_north_carolina, aes(x = locationLongitude, y = locationLatitude), color = "white", size = 1,inherit.aes = FALSE)
-
-nc_pov_plot + scale_fill_gradientn(colours = rev(rainbow(7))) + # overwriting with easier to read color set
-  geom_point(data= opps_north_carolina, aes(x = locationLongitude, y = locationLatitude), color = "black", size = 2,inherit.aes = FALSE)
+add_data_plot("north carolina", nc_pov,"By Poverty Rate")
 
 # as we can see, the red/yellow and green counties with the highest poverty rates have the least offerings of opportunities
 
+nc_white <- read.csv("data/north_carolina_non_white.csv", colClasses=c("NULL", NA, NA))
+add_data_plot("north carolina", nc_white, "By Non White Population")
+
+# it does not appear, that non-white people have that clear disadvantage as poor people have in terms of opportunity availability
+
+nc_population <- read.csv("data/north_carolina_population.csv",colClasses=c("NULL", NA, NA))
+add_data_plot("north carolina", nc_population, "By Population")
+
+# and as suspected, opportunities seems to correlate with population
+
+ohio_pop <- read.csv("data/ohio_population.csv",colClasses=c("NULL", NA, NA))
+add_data_plot("ohio", ohio_pop, "By Population")
+
+
+indiana_pop <-  read.csv("data/indiana_population.csv",colClasses=c("NULL", NA, NA))
+add_data_plot("indiana", indiana_pop, "By Population")
+
+# check on the one point in the south-west, where population is low bit there are still 3 opps
+opps_inspect %>% filter(locationLongitude > -87.2 & locationLongitude < -86.7 & locationLatitude >38.7 & locationLatitude < 39) %>% 
+  select(locationCity, locationState, locationLatitude, locationLongitude)
+# the City is Odon, Indiana
+# and on the north east point
+opps_inspect %>% filter(locationLongitude > -85.7 & locationLongitude < -85.5 & locationLatitude >40.5 & locationLatitude < 41.5) %>% 
+  select(locationCity, locationState, locationLatitude, locationLongitude)
+# which is South Whitley, Indiana
+
+# I can#t find anything specific about these two cities which makes them stand out
+
+texas_population <- read.csv("data/texas_population.csv",colClasses=c("NULL", NA, NA))
+add_data_plot("texas", texas_population, "By Population")
 
